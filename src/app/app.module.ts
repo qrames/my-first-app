717fabcefad4d6c5a78ccd7d356990403cb06f11
qrames <ramque@orange.fr>
2018-11-20 09:30:51 +0100

    the routing work scss change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { LoosersComponent } from './loosers/loosers.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     LoosersComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
